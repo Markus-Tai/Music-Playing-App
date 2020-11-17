e7d7203880d9bcd198e9d62f2bfb60493c3ee1e3
MarkusTai <markust0112@gmail.com>
2020-11-17 11:34:52 -0800

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.musicplayer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String song1Name = "Song 1";
+        String songArtist = "Artist 1";
+        int song1ImageResource;
+        int song1Mp3Resource;
+
+        //Arrays
+
+    }
+}
